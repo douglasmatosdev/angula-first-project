1004877d9d681fb110b73da989d113f10012ad18
douglasmatosdev <douglasmatosdev@gmail.com>
2022-10-09 12:35:10 -0300

    props - data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  userName = 'John Doe'
+
+  userData = {
+    email: "john.doe@email.com",
+    role: "admin"
+  }
+
   title = 'angula-first-project';
 }
